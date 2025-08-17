bf44b5a13a7d7744620cc1276fe868da16f6b919
HaiovyiDmytro <gdv_krot@yahoo.com>
2025-08-17 09:14:28 +0200

    add threads naming

---
+++ 3_ThreadsNaming/1_ThreadsNaming/Program.cs
@@ -0,0 +1,30 @@
+﻿/// <summary>
+/// Поток можно поименовать, используя свойство Name.
+/// Это предоставляет большое удобство при отладке: имена потоков можно вывести в Console.WriteLine
+/// и увидеть в окне Debug – Threads в Microsoft Visual Studio.
+/// Имя потоку может быть назначено в любой момент.
+/// </summary>
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Thread thread = new Thread(() =>
+        {
+            Thread.CurrentThread.Name = "@lambda";
+            Console.WriteLine("Hello from " + Thread.CurrentThread.Name);
+        });
+        Thread worker = new Thread(Go)
+        {
+            Name = "worker"
+        };
+        worker.Start();
+        thread.Start();
+        Thread.CurrentThread.Name = "main";
+        Go();
+    }
+
+    private static void Go()
+    {
+        Console.WriteLine("Hello from " + Thread.CurrentThread.Name);
+    }
+}
