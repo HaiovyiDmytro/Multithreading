7aa8c2510e7282f9a676f952baf4c9a48aedf2f3
HaiovyiDmytro <gdv_krot@yahoo.com>
2025-08-23 07:53:45 +0200

    add IsBackground

---
+++ 4_BackgroundThreads/1_BackgroundThreads/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Thread worker = new Thread(delegate ()
+        {
+            Console.ReadLine();
+        });
+
+        if (args.Length == 0)
+        {
+            worker.IsBackground = false;
+        }
+        else
+        {
+            worker.IsBackground = true;
+        }
+
+        worker.Start();
+    }
+}
