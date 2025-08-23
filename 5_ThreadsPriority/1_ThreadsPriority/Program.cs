c590303f8aa372805bee80015d7848fc14b12796
HaiovyiDmytro <gdv_krot@yahoo.com>
2025-08-23 08:16:06 +0200

    add ThreadsPriority

---
+++ 5_ThreadsPriority/1_ThreadsPriority/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    /// <summary>
+    /// Свойство Priority определяет, сколько времени на исполнение будет выделено потоку 
+    /// относительно других потоков того же процесса. Существует 5 градаций приоритета потока:
+    /// enum ThreadPriority { Lowest, BelowNormal, Normal, AboveNormal, Highest }
+    /// </summary>
+    /// <param name="args"></param>
+    private static void Main(string[] args)
+    {
+        // Значение приоритета становится существенным, когда одновременно исполняются несколько потоков.
+        // Установка приоритета потока на максимум еще не означает работу в реальном времени(real-time),
+        // так как существуют еще приоритет процесса приложения.
+        // Чтобы работать в реальном времени, нужно использовать класс Process из пространства имен
+        // System.Diagnostics для поднятия приоритета процесса
+        Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.High; // ProcessPriorityClass.RealTime;
+
+
+        Console.WriteLine("Hello, World!");
+    }
+}
